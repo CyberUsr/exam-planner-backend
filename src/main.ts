a9ed304420d7d5f73139250f5b49b739cbd8813a
Ghidoarca Petru_Cristi <petru.ghidoarca@student.usv.ro>
2024-11-19 08:49:01 +0200

    corse connection

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  //Enable cors
+  app.enableCors({
+    origin: 'http://localhost:3001', // The frontend's origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3003);
 
   // Swagger setup
   const config = new DocumentBuilder()
