2a47e20e1b0776b0c0e5c6d2f469afdae5107d3f
Blacktrecks <petricaroman815@gmail.com>
2024-11-20 17:26:38 +0200

    Add cereri logic

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: 'http://localhost:3001', // Frontend origin
+    origin: 'http://localhost:3000', // Frontend origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
