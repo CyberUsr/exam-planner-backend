df14f1d9099e5d4e748392178d135e07ee89f2cc
Ghidoarca Petru_Cristi <petru.ghidoarca@student.usv.ro>
2024-11-20 14:38:39 +0200

    authentification

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+  async register(email: string, password: string) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prisma.user.create({
+      data: { email, password: hashedPassword },
+    });
+
+    return { message: 'User registered successfully!', user };
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const token = this.jwtService.sign({ userId: user.id });
+    return { message: 'Logged in successfully!', token };
+  }
+}
