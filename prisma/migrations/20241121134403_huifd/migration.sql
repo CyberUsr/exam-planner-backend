e78a59c3d7be4ca78e66a9d17533c71b0b25566b
Ghidoarca Petru_Cristi <petru.ghidoarca@student.usv.ro>
2024-11-23 15:32:08 +0200

    refactoring the user table and its module so it will diferentiate the user between profesori and studenti

--- prisma/migrations/20241121134403_huifd/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `departamente` on the `Sali` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Sali" DROP COLUMN "departamente";
--- src/dto/register.dto.ts
+++ src/dto/register.dto.ts
@@ -1,13 +1,29 @@
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, MinLength ,IsBoolean, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterDto {
-  @ApiProperty({ example: 'user@example.com', description: 'Your email address' })
+  @ApiProperty({ 
+    example: 'user@usm.ro', 
+    description: 'Your email address. Professors should use emails ending with @usm.ro.' 
+  })
   @IsEmail()
   email: string;
 
-  @ApiProperty({ example: 'password123', description: 'Your password', minLength: 6 })
+  @ApiProperty({ 
+    example: 'password123', 
+    description: 'Your password. Must be at least 6 characters long.', 
+    minLength: 6 
+  })
   @IsNotEmpty()
   @MinLength(6)
   password: string;
+
+  
+  @ApiProperty({ 
+    example: true, 
+    description: 'Indicates if the user is a professor. This field is derived from the email domain.' 
+  })
+  @IsBoolean()
+  @IsOptional()
+  isProfesor?: boolean;
 }
