0cb5e0f3d34401ea762fbd719c5b435c59ee06cd
Blacktrecks <petricaroman815@gmail.com>
2024-12-16 17:07:24 +0200

    pool from dovahkiin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ProfesoriModule } from './profesori/profesori.module';
 import { PrismaModule } from './prisma/prisma.module';
